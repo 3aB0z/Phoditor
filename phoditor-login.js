7eaf26e6393d9e2ab02ce1580780c411fdd605ea
3aB0z <3aB0zloukhmi@gmail.com>
2024-01-16 16:07:16 +0100

    design was added

---
+++ phoditor-login.js
@@ -0,0 +1,10 @@
+users = ['loukhmi@gmail.com', 'loukhmi']
+
+let email = document.getElementById(stemail)
+
+let password = document.getElementById(stpassword)
+
+if (email == users[0] && password == users[1]) {
+    alert('Logged Successfully!')
+}
+
---
+++ phoditor-sign-up.js
@@ -0,0 +1,17 @@
+users = []
+
+let stusername = document.getElementById(username);
+
+let stemail = document.getElementById(email);
+
+let stpassword = document.getElementById(password);
+
+user = [stusername, stemail, stpassword]
+
+users.push(user);
+
+
+
+
+
+
